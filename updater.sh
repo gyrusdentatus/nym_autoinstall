112aaf26366f0856e8131d624cfd5a19d3817016
Hans Bricks <33793809+gyrusdentatus@users.noreply.github.com>
2020-11-24 21:09:57 +0100

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -7,15 +7,9 @@ WHITE='\033[1;97m' # LARGER FONT
 LBLUE='\033[1;96m' # HIGHLIGHTS / NUMBERS ...
 LGREEN='\033[1;92m' # SUCCESS
 NOCOLOR='\033[0m' # DEFAULT FONT
-
-function systemd_ison () {
-if systemctl list-units --state=running | grep nym-mixnode
-then echo "stopping nym-mixnode.service to update the node ..." && systemctl stop nym-mixnode
-else echo " nym-mixnode.service is inactive or not existing. Downloading new binaries ..."
-fi
-}
+current_version=$(./nym-mixnode_linux_x86_64 --version | grep Nym | cut -c 13- )
 function downloader () {
-#set -x
+set -x
 if [ ! -d /home/nym/.nym/mixnodes ]
 then
 	echo "Looking for nym config in /home/nym but could not find any! Enter the path of the nym-mixnode executable"
@@ -28,6 +22,7 @@ else
 fi
 
 # set vars for version checking and url to download the latest release of nym-mixnode
+current_version=$(./nym-mixnode_linux_x86_64 --version | grep Nym | cut -c 13- )
 VERSION=$(curl https://github.com/nymtech/nym/releases/latest --cacert /etc/ssl/certs/ca-certificates.crt 2>/dev/null | egrep -o "[0-9|\.]{5}(-\w+)?")
 URL="https://github.com/nymtech/nym/releases/download/v$VERSION/nym-mixnode_linux_x86_64"
 
@@ -49,7 +44,7 @@ else
 fi
 }
 function upgrade_nym () {
-#set -x
+set -x
 cd /home/nym
 select d in /home/nym/.nym/mixnodes/* ; do test -n "$d" && break; printf "%b\n\n\n" "${WHITE} >>> Invalid Selection"; done
 directory=$(echo "$d" | rev | cut -d/ -f1 | rev)
@@ -62,12 +57,12 @@ printf "%b\n\n\n"
 printf "%b\n\n\n" "${WHITE} Address for the incentives rewards will be ${YELLOW} ${wallet} "
 printf "%b\n\n\n" "${WHITE} You may later change it in config.toml if needed, but you need to stop the node first and then edit it with an editor such as nano"
 
-current_version=$(./nym-mixnode_linux_x86_64 --version | grep Nym | cut -c 13- )
+#current_version=$(./nym-mixnode_linux_x86_64 --version | grep Nym | cut -c 13- )
 #printf "%b\n\n\n" "${WHITE} Enter your current version of the mixnode - make sure the format is right. Example input:0.8.1"
 #read current_version
 #printf "%b\n\n\n" "${WHITE} You entered version ${current_version}"
 sudo -u nym -H ./nym-mixnode_linux_x86_64 upgrade --id $directory --incentives-address $wallet --current-version $current_version
 }
-#set -x
+set -x
 downloader && echo "ok" && sleep 2 || exit 1
 upgrade_nym && sleep 5 && systemctl start nym-mixnode.service
